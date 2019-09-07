9a5f391160ad27ea88a15ecf5abbdeb9a336c2ab
Sam <shimuel@yahoo.com>
2019-09-07 17:22:39 -0600

    Amend

---
+++ Startup.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using GraphQL;
+using GraphiQl;
+
+using GraphQL.Types;
+
+
+namespace GraphQLTest
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IDependencyResolver>(s => new FuncDependencyResolver(s.GetRequiredService));
+
+            services.AddSingleton<IDocumentExecuter, DocumentExecuter>();
+            //services.AddSingleton<IDocumentWriter, DocumentWriter>();
+                        
+            services.AddSingleton<NorthwindQuery>();
+            services.AddSingleton<NorthwindMutation>();
+            services.AddSingleton<OrderDetailType>();
+            services.AddSingleton<OrderType>();
+            services.AddSingleton(typeof(PaginatedGraphType<>));
+            services.AddSingleton(typeof(PageType<,>));
+            services.AddSingleton<ISchema, NorthwindSchema>();
+            services.AddMvc();
+            
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            
+            app.UseGraphiQl("/graphql");
+            app.UseMvc();
+        }
+    }
+}
